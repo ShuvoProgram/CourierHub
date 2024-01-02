a3e207d90423179802e19b3d574b8159efc0c26a
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-02 23:42:15 +0600

    Added Sidebar with Drawer component 🎨.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import SidebarWithHeader from './components/SidebarWithHeader/SidebarWithHeader';
 // import { RouterProvider } from 'react-router-dom';
 // import routes from './routes/routes';
 import SIgnIn from './pages/SignIn/SIgnIn';
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
       {/* <RouterProvider router={routes}/> */}
-      <SIgnIn/>
+      {/* <SIgnIn/> */}
+      <SidebarWithHeader/>
     </>
   )
 }
