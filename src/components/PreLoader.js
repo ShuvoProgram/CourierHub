f64e1730e55f8716268b4273825f2d5dfe67a52a
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-03 00:52:03 +0600

    Add PreLoader component with logo, progress bar, and text. 🚀

---
+++ src/components/PreLoader.js
@@ -0,0 +1,24 @@
+import logo from '../assets/logo.svg';
+import { Image, Progress, VStack, Text, HStack } from '@chakra-ui/react';
+
+function PreLoader() {
+  return (
+    <VStack
+      w='100%'
+      h='100vh'
+      spacing='4'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Image src={logo} width='150px' />
+      <Progress size='xs' w='40%' colorScheme='brown' isIndeterminate />
+      <HStack>
+        <Text fontWeight='300' color='gray.400'>
+          search less, wear more
+        </Text>
+      </HStack>
+    </VStack>
+  );
+}
+
+export default PreLoader;
